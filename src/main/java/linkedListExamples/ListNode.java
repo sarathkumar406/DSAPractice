14a60d17a520f9dec9abde088d4bc4b680319a1c
Sarath <sarathkumar.406@gmail.com>
2026-04-26 17:12:43 +0530

    Reverse LinkedList

---
+++ src/main/java/linkedListExamples/ListNode.java
@@ -0,0 +1,10 @@
+package linkedListExamples;
+
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val = val;
+    }
+}
---
+++ src/main/java/linkedListExamples/ReverseList.java
@@ -0,0 +1,33 @@
+package linkedListExamples;
+
+public class ReverseList {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+
+        ReverseList reverseList = new ReverseList();
+        ListNode node = reverseList.reveserList(head);
+
+        while(node!=null){
+            System.out.print(node.val+ " ");
+            node = node.next;
+        }
+    }
+
+    private ListNode reveserList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while(curr!=null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
